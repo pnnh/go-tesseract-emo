6edb4480c1292d19380f96647320e9d16d6221b9
larry <xspanni@gmail.com>
2022-09-30 17:27:24 +0800

    处理webp图片 调整重命名后的图片名字格式，避免处理手动修改过的图片

--- bin/main.go
+++ bin/main.go
@@ -19,7 +19,7 @@ var flagdir string
 var letterRunes = []rune("0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")
 
 func init() {
-	flag.StringVar(&flagdir, "dir", "", "指定目录")
+	flag.StringVar(&flagdir, "dir", "~/CloudDocs/图片/表情图片/沙雕", "指定目录")
 	flag.Parse()
 	logrus.SetLevel(logrus.DebugLevel)
 	rand.Seed(time.Now().UnixNano())
@@ -55,20 +55,32 @@ func loopDir(rootPath string) {
 		logrus.Fatalln("ReadDir error:", err)
 	}
 	for _, f := range files {
-		fileExt := strings.ToLower(filepath.Ext(f.Name()))
-		fullPath := rootPath + "/" + f.Name()
-		logrus.Debugln("====: ", fullPath, f.Name(), fileExt)
 		//if f.IsDir() {
 		//	loopDir(fullPath)
 		//	continue
 		//}
+		fileName := f.Name()
+		fileExt := strings.ToLower(filepath.Ext(f.Name()))
+		hansIndex := strings.IndexFunc(fileName, func(r rune) bool {
+			if unicode.Is(unicode.Han, r) {
+				return true
+			}
+			return false
+		})
+		logrus.Debugln("HasSuffix: ", hansIndex, strings.TrimRight(fileName, fileExt), strings.HasSuffix(strings.TrimRight(fileName, fileExt), "-emo"))
+		//if hansIndex >= 0 && !strings.HasSuffix(strings.TrimRight(fileName, fileExt), "-emo") {
+		//	// 不处理手动修改过的文件
+		//	continue
+		//}
+		fullPath := rootPath + "/" + f.Name()
+		logrus.Debugln("====: ", fullPath, f.Name(), fileExt)
 		if fileExt == ".png" || fileExt == ".jpg" || fileExt == ".jpeg" ||
-			fileExt == ".gif" {
+			fileExt == ".gif" || fileExt == ".webp" {
 			logrus.Debugln("++++: ", f.Name(), fileExt)
 			ocrText := runTesseract(fullPath)
 			logrus.Debugln("----: ", ocrText)
 			if len(ocrText) > 0 {
-				newPath := rootPath + "/emo-" + RandStringRunes(8) + "-" + ocrText + fileExt
+				newPath := rootPath + "/" + ocrText + "-" + RandStringRunes(8) + "-emo" + fileExt
 				logrus.Debugln("move: ", newPath)
 				err := os.Rename(fullPath, newPath)
 				if err != nil {
